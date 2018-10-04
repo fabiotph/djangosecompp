887011bd6e1ac2ad6963a0c16406ad0a51669709
Fabiotph <fabiotperez@gmail.com>
2018-10-04 14:17:10 -0300

    adicionado pythonanywhere ao allowed hosts

--- djangosecompp/settings.py
+++ djangosecompp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@vk#wgo2$g2)jdmwmr1ti1)=wlh-vvtwuc+*&rpw13q#9h6!^k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
